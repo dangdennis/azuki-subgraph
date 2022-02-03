7e05eb531a04b420b4d618294b8caca1fbd8cd03
Dennis Dang <dangggdennis@gmail.com>
2022-02-02 23:45:51 -0500

    manually import azuki attribute

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,24 +6,24 @@ import {
 	OwnershipTransferred,
 	Transfer,
 } from "../generated/azuki/azuki";
-import { ExampleEntity } from "../generated/schema";
+import { Azukis } from "../generated/schema";
 
 export function handleApproval(event: Approval): void {
 	// Entities can be loaded from the store using a string ID; this ID
 	// needs to be unique across all entities of the same type
-	let entity = ExampleEntity.load(event.transaction.from.toHex());
+	let entity = Azukis.load(event.transaction.from.toHex());
 
 	// Entities only exist after they have been saved to the store;
 	// `null` checks allow to create entities on demand
 	if (!entity) {
-		entity = new ExampleEntity(event.transaction.from.toHex());
+		entity = new Azukis(event.transaction.from.toHex());
 
 		// Entity fields can be set using simple assignments
 		entity.count = BigInt.fromI32(0);
 	}
 
 	// BigInt and BigDecimal math are supported
-	entity.count = entity.count + BigInt.fromI32(1);
+	entity.count = BigInt.fromI32(1).plus(entity.count);
 
 	// Entity fields can be set based on event parameters
 	entity.owner = event.params.owner;
